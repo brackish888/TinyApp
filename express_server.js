6b7da5829cb99876618a6023dbb6c673817cacf3
Sam T <bbrackish@protonmail.com>
2021-03-16 04:14:46 +0000

    redirected the user to the page of the longUrl via the new shortUrl

--- express_server.js
+++ express_server.js
@@ -2,6 +2,29 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+const getRandomChar = (string) => {
+  return Math.floor(Math.random() * string.length);
+};
+
+const generateRandomString = (callback) => {
+  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy';
+  let randomString = '';
+  for (let i = 0; i < 6; i++) {
+    randomString += letters[callback(letters)];
+  }
+  return randomString;
+};
+// cite https://coderrocketfuel.com/article/generate-a-random-letter-from-the-alphabet-using-javascript
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Logs the POST request body to the console
+  const shortURL = generateRandomString(getRandomChar);
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);       
+});
+
 app.set("view engine", "ejs");
 //Tells Express to use EJS as engine
 
@@ -9,6 +32,8 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -18,19 +43,26 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.longURL] };
   res.render("urls_show", templateVars);
 });
